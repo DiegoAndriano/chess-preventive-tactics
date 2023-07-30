deae3815bd839950213abcb075f892cb97fb7e18
josefina <josefina.barrera@live.com.ar>
2023-07-30 18:19:12 -0300

    End of MVP

--- resources/js/components/Board.vue
+++ resources/js/components/Board.vue
@@ -1,6 +1,6 @@
 <script setup>
-import {onMounted, ref, computed} from 'vue'
-import {TheChessboard} from 'vue3-chessboard';
+import { onMounted, ref, computed } from 'vue'
+import { TheChessboard } from 'vue3-chessboard';
 import 'vue3-chessboard/style.css';
 import IconChevronLeft from './Icons/IconChevronLeft.vue'
 import IconChevronRight from './Icons/IconChevronRight.vue'
@@ -8,7 +8,7 @@ import IconCheck from './Icons/IconCheck.vue'
 import IconCross from './Icons/IconCross.vue'
 import IconAndina from './Icons/IconAndina.vue'
 
-const props = defineProps({tactic: Array})
+const props = defineProps({ tactic: Array })
 
 let boardAPI;
 
@@ -70,31 +70,32 @@ function prev() {
 }
 
 function checkAnswer(answer) {
-    submitted.value = true
     axios.get(games[index.value].answer_path).then((response) => {
         if (answer === response.data) {
             success.value = true
+            submitted.value = true
         } else {
             success.value = false
+            submitted.value = true
         }
     })
 }
 
-function rateEasy(){
-    if(!rated.value){
+function rateEasy() {
+    if (!rated.value) {
         rated.value = true
         axios.get(games[index.value].easy_path)
     }
 
 }
-function rateMedium(){
-    if(!rated.value){
+function rateMedium() {
+    if (!rated.value) {
         rated.value = true
         axios.get(games[index.value].medium_path)
     }
 }
-function rateHard(){
-    if(!rated.value){
+function rateHard() {
+    if (!rated.value) {
         rated.value = true
         axios.get(games[index.value].hard_path)
     }
@@ -103,79 +104,86 @@ function rateHard(){
 </script>
 <template>
     <div class="bg-stone-50 dark:bg-stone-900">
-        <TheChessboard class="pb-4" :board-config="boardConfig" @board-created="(api) => (boardAPI = api)"/>
+        <div class="flex flex-col xl:flex-row xl:gap-10 xl:px-24">
+            <div>
+                <TheChessboard class="pb-4" :board-config="boardConfig" @board-created="(api) => (boardAPI = api)" />
+            </div>
 
-        <div class="flex gap-x-16 px-4 py-6 md:px-10">
-            <button
-                :disabled="submitted"
-                @click="checkAnswer(option_one)"
-                class="h-auto w-24 bg-stone-300 hover:bg-stone-400 hover:shadow-sm focus:shadow-sm focus:bg-[#8C8783] focus:font-bold px-4 py-2 rounded-lg font-semibold text-stone-700 transform duration-100 ease-in-out dark:bg-stone-900 dark:border dark:text-stone-100 dark:border-stone-50 dark:hover:bg-[#2C2927] md:">
-                {{ option_one }}
-            </button>
-            <button
-                :disabled="submitted"
-                @click="checkAnswer(option_two)"
-                class="h-auto w-24 bg-stone-300  hover:bg-stone-400 hover:shadow-sm focus:shadow-sm focus:bg-[#8C8783] focus:font-bold px-4 py-2 rounded-lg font-semibold text-stone-700 transform duration-100 ease-in-out dark:bg-stone-900 dark:border dark:text-stone-100 dark:border-stone-50 dark:hover:bg-[#2C2927]">
-                {{ option_two }}
-            </button>
-            <button v-if="option_three"
-                    :disabled="submitted"
-                    @click="checkAnswer(option_three)"
-                    class="h-auto w-24 bg-stone-300  hover:bg-stone-400 hover:shadow-sm focus:shadow-sm focus:bg-[#8C8783] focus:font-bold px-4 py-2 rounded-lg font-semibold text-stone-700 transform duration-100 ease-in-out  dark:bg-stone-900 dark:border dark:text-stone-100 dark:border-stone-50 dark:hover:bg-[#2C2927]">
-                {{ option_three }}
-            </button>
+            <div class="flex flex-col w-full px-6 md:px-10 lg:px-24 xl:px-0 gap-y-6 md:gap-y-10 py-6 md:py-10 xl:pt-0">
+                <div class="flex gap-x-16 xl:gap-x-16 py-6 xl:px-0 lg:pt-16 lg:pb-16 xl:pt-0">
+                    <button :disabled="submitted" @click="checkAnswer(option_one)"
+                        class="h-auto w-24 bg-stone-300 hover:bg-stone-400 hover:shadow-sm focus:shadow-sm focus:bg-[#8C8783] focus:font-bold px-4 py-2 rounded-lg font-semibold text-stone-700 transform duration-100 ease-in-out dark:bg-stone-900 dark:border dark:text-stone-100 dark:border-stone-50 dark:hover:bg-[#2C2927] lg:h-16 lg:w-24 lg:text-2xl">
+                        {{ option_one }}
+                    </button>
+                    <button :disabled="submitted" @click="checkAnswer(option_two)"
+                        class="h-auto w-24 bg-stone-300  hover:bg-stone-400 hover:shadow-sm focus:shadow-sm focus:bg-[#8C8783] focus:font-bold px-4 py-2 rounded-lg font-semibold text-stone-700 transform duration-100 ease-in-out dark:bg-stone-900 dark:border dark:text-stone-100 dark:border-stone-50 dark:hover:bg-[#2C2927] h-16 lg:w-24 lg:text-2xl">
+                        {{ option_two }}
+                    </button>
+                    <button v-if="option_three" :disabled="submitted" @click="checkAnswer(option_three)"
+                        class="h-auto w-24 bg-stone-300  hover:bg-stone-400 hover:shadow-sm focus:shadow-sm focus:bg-[#8C8783] focus:font-bold px-4 py-2 rounded-lg font-semibold text-stone-700 transform duration-100 ease-in-out  dark:bg-stone-900 dark:border dark:text-stone-100 dark:border-stone-50 dark:hover:bg-[#2C2927] lg:h-16 lg:w-24 lg:text-2xl">
+                        {{ option_three }}
+                    </button>
+                </div>
+                <div v-if="submitted && success"
+                    class="w-auto h-auto px-4 py-4 gap-4 flex bg-emerald-50 text-stone-900 border border-emerald-700 rounded-lg dark:bg-emerald-950 dark:text-stone-50 lg:text-2xl lg:items-center md:w-[400px]">
+                    <IconCheck class="w-6 h-6 lg:w-10 lg:h-10 text-emerald-700 dark:text-emerald-400"></IconCheck>
+                    <p class="dark:text-stone-50">Congrats!</p>
+                </div>
+                <div v-if="submitted && !success"
+                    class="w-full h-auto px-4 py-4 gap-4 flex bg-rose-50 border border-rose-700 dark:bg-rose-950 rounded-lg lg:text-2xl lg:items-center md:w-[400px]">
+                    <IconCross class="w-6 h-6 lg:w-10 lg:h-10 text-rose-700 dark:text-rose-400"></IconCross>
+                    <p class="dark:text-stone-50">Keep working!</p>
+                </div>
+                <p v-if="submitted" class="dark:text-stone-100 pb-4 break-all lg:text-xl lg:w-[400px]">
+                    {{ games[index].description }}
+                </p>
+
+            </div>
+
         </div>
 
-        <div class="flex justify-between items-center px-4 py-6 md:px-10">
+        <div v-if="submitted" class="w-auto h-auto flex flex-col py-6 md:py-10 items-baseline lg:px-24 px-6">
+            <h3 class="text-stone-900 font-semibold md:text-lg lg:text-xl py-4">How hard did you find this puzzle?</h3>
+            <div class="w-full flex justify-between items-baseline">
+                <div @click="rateEasy"
+                    class="group cursor-pointer w-auto h-auto px-2 py-2 gap-4 flex flex-col items-center rounded-lg md:m-10 lg:text-xl">
+                    <IconAndina
+                        class="transform duration-200 ease-in-out group-hover:text-stone-600 group-hover:dark:text-stone-500 dark:text-stone-100 w-8 h-8 md:w-16 md:h-16">
+                    </IconAndina>
+                    <p class="dark:text-stone-100">Easy</p>
+                </div>
+                <div @click="rateMedium"
+                    class="group cursor-pointer w-auto h-auto  px-2 py-2 gap-4 flex flex-col items-center rounded-lg md:m-10 lg:text-xl">
+                    <IconAndina
+                        class="transform duration-200 ease-in-out group-hover:text-stone-600 group-hover:dark:text-stone-500 dark:text-stone-100 w-12 h-12 md:w-24 md:h-24 ">
+                    </IconAndina>
+                    <p class="dark:text-stone-100">Medium</p>
+                </div>
+                <div @click="rateHard"
+                    class="group cursor-pointer w-auto h-auto  px-2 py-2 gap-4 flex flex-col rounded-lg items-center md:m-10 lg:text-xl">
+                    <IconAndina
+                        class="transform duration-200 ease-in-out group-hover:text-stone-600 group-hover:dark:text-stone-500 dark:text-stone-100 w-16 h-16 md:w-28 md:h-28 ">
+                    </IconAndina>
+                    <p class="dark:text-stone-100">Hard</p>
+                </div>
+            </div>
+        </div>
+
+        <p v-if="rated" class="m-6 text-green-600 font-semibold text-sm lg:text-xl">Thank you for rating!</p>
+
+        <div class="flex justify-between items-center px-4 py-6 md:px-10 lg:pb-6 lg:px-20">
             <button @click="prev"
-                    class=" w-auto h-auto flex items-center gap-4 pl-1 pr-4 py-2 rounded-lg text-stone-500 hover:bg-stone-200 hover:shadow-sm  dark:text-stone-100 dark:hover:bg-[#2C2927]">
+                class=" w-auto h-auto flex items-center gap-4 pl-1 pr-4 py-2 rounded-lg text-stone-500 hover:bg-stone-200 hover:shadow-sm  dark:text-stone-100 dark:hover:bg-[#2C2927] lg:h-auto lg:w-auto lg:pl-4 lg:pr-8 lg:py-4 lg:text-2xl">
                 <span>
                     <IconChevronLeft class="w-6 h-6"> </IconChevronLeft>
                 </span>Previous game
             </button>
             <button @click="next"
-                    class=" w-auto h-auto flex items-center gap-4 pl-4 pr-1 py-2 rounded-lg hover:bg-stone-200 hover:shadow-sm text-stone-500  dark:text-stone-100 dark:hover:bg-[#2C2927]">
+                class=" w-auto h-auto flex items-center gap-4 pl-4 pr-1 py-2 rounded-lg hover:bg-stone-200 hover:shadow-sm text-stone-500  dark:text-stone-100 dark:hover:bg-[#2C2927] lg:h-auto lg:w-auto lg:pr-4 lg:pl-8 lg:py-4 lg:text-2xl">
                 Next game <span>
                     <IconChevronRight class="w-6 h-6"> </IconChevronRight>
                 </span>
             </button>
-        </div>
-
-        <div v-if="submitted">
-            <div>
-                <div
-                    v-if="success"
-                    class="w-auto h-auto m-6 px-6 py-4 gap-4 flex bg-emerald-50 border border-emerald-700 rounded-lg dark:bg-stone-800 dark:text-stone-50 md:m-10">
-                    <IconCheck class="w-6 h-6"></IconCheck>
-                    <p>Congrats!</p>
-                </div>
-                <div
-                    v-if="!success"
-                    class="w-auto h-auto m-6 px-6 py-4 gap-4 flex bg-rose-50 border border-rose-700 rounded-lg md:m-10">
-                    <IconCross class="w-6 h-6"></IconCross>
-                    <p>Keep working!</p>
-                </div>
-                <p class="px-6 pb-4 md:px-10 break-all">
-                    {{ games[index].description }}
-                </p>
-            </div>
-
-            <div class="w-auto h-auto flex m-6 items-baseline justify-evenly">
-                <div @click="rateEasy" class="cursor-pointer w-auto h-auto px-2 py-2 gap-4 flex flex-col items-center rounded-lg md:m-10">
-                    <IconAndina class="w-8 h-8"></IconAndina>
-                    <p>Easy</p>
-                </div>
-                <div @click="rateMedium" class="cursor-pointer w-auto h-auto  px-2 py-2 gap-4 flex flex-col items-center rounded-lg md:m-10">
-                    <IconAndina class="w-12 h-12"></IconAndina>
-                    <p>Medium</p>
-                </div>
-                <div @click="rateHard" class="cursor-pointer w-auto h-auto  px-2 py-2 gap-4 flex flex-col rounded-lg items-center md:m-10">
-                    <IconAndina class="w-16 h-16"></IconAndina>
-                    <p>Hard</p>
-                </div>
-            </div>
-            <p v-if="rated" class="m-6 text-green-600 font-semibold text-sm">Thank you for rating!</p>
-
         </div>
 
     </div>
