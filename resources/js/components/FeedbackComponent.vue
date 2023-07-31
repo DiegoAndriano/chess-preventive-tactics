dad3d8059908b3a3ac06d585e217e0f01f7e1059
josefina <josefina.barrera@live.com.ar>
2023-07-31 16:34:28 -0300

    layout 31

--- resources/js/components/FeedbackComponent.vue
+++ resources/js/components/FeedbackComponent.vue
@@ -1,7 +1,8 @@
 <script setup>
-import {reactive} from 'vue'
+import { reactive } from 'vue'
 import IconEdit from './Icons/IconEdit.vue'
-import {useTacticStore} from "../stores/tactic";
+import { useTacticStore } from "../stores/tactic";
+import IconClose from './Icons/IconClose.vue';
 
 const tactic = useTacticStore()
 
@@ -46,28 +47,43 @@ function submit() {
 <template>
     <div class="m-4 flex justify-end md:m-10 lg:m-20">
         <button @click="tactic.toggleFeedbackModal"
-                class="flex cursor-pointer items-center gap-4 w-auto px-4 py-2 bg-stone-50  rounded-lg text-stone-500 shadow-sm hover:bg-stone-200 focus:bg-stone-300 focus:font-semibold  dark:bg-stone-900 dark:text-stone-200 dark:hover:bg-[#2C2927] lg:text-xl lg:px-8 lg:py-4">
+            class="flex cursor-pointer items-center gap-4 w-auto px-4 py-2 bg-stone-50  rounded-lg text-stone-500 shadow-sm hover:bg-stone-200 focus:bg-stone-300 focus:font-semibold  dark:bg-stone-950 dark:text-stone-200 dark:hover:bg-[#2C2927] lg:text-xl lg:px-8 lg:py-4">
             Submit feedback
             <IconEdit class="w-6 h-6 lg:w-auto lg:h-8 dark:text-stone-100"></IconEdit>
         </button>
     </div>
     <transition name="slide">
+        <div
+         v-if="tactic.modal.feedback"
+         class="bg-stone-500/40 h-full w-full fixed top-0 z-10"></div>
+    </transition>
+    <transition name="slide">
         <form v-if="tactic.modal.feedback"
-              class="fixed w-full h-[500px] bg-white z-50 px-6 py-10 top-0 bottom-0 my-auto border border-4 border-stone-900 flex flex-col">
-            <div class="flex justify-end py-4">
-                <button @click.prevent="tactic.toggleFeedbackModal" class="text-red-600 cursor-pointer">X</button>
+            class="z-50 fixed rounded-lg m-10 w-[320px] h-[500px] bg-white z-50 px-4 py-4 top-0 bottom-0 my-auto mx-auto left-0 right-0 border border-1 border-stone-200 shadow-sm flex flex-col dark:bg-stone-800 dark:border-stone-700">
+            <div class="flex justify-end items-baseline py-4 gap-4">
+                <p class="text-sm dark:text-stone-100">Thanks for taking the time!! Let us know what you think...</p>
+                <button @click.prevent="tactic.toggleFeedbackModal" class="text-stone-700 dark:text-stone-100 cursor-pointer">
+                    <IconClose class="w-6 h-6"></IconClose>
+                </button>
             </div>
-            <div class="flex flex-col gap-y-6">
+            <div class="flex flex-col h-full pb-8">
                 <input @focus="error.author = false" @blur="validateAuthor" v-model="form.author"
-                       :class="error.author !== false ? 'border-red-600' : ''"
-                       class="border border-4 border-stone-900 px-4 py-2"/>
-                <p v-if="error.author !== false" class="text-red-500 text-sm">{{ error.author }}</p>
-                <input @focus="error.feedback = false" @blur="validateFeedback" v-model="form.feedback"
-                       :class="error.feedback !== false ? 'border-red-600' : ''"
-                       class="border border-4 border-stone-900 px-4 py-2"/>
-                <p v-if="error.feedback !== false" class="text-red-500 text-sm">{{ error.feedback }}</p>
-                <button @click.prevent="submit" class="border border-4 border-stone-900 px-4 py-2">Enviar</button>
+                    :class="error.author !== false ? 'border-rose-600 mb-2' : ''"
+                    class="border border-stone-300 rounded-lg px-4 py-2 bg-stone-200 placeholder:text-stone-400 text-stone-700 text-sm outline-none dark:bg-stone-700 dark:border-stone-500 dark:text-stone-100"
+                    placeholder="Name" />
+                <p v-if="error.author !== false" class="text-rose-500 text-xs">{{ error.author }}</p>
+                <textarea @focus="error.feedback = false" @blur="validateFeedback" v-model="form.feedback"
+                    :class="error.feedback !== false ? 'border-rose-600 mb-2' : ''" rows="8" cols="10"
+                    class="my-6 border border-stone-300 rounded-lg px-4 py-2 bg-stone-200 placeholder:text-stone-400 text-stone-700 text-sm outline-none
+                    dark:bg-stone-700 dark:border-stone-500 dark:text-stone-100"
+                    placeholder="Message"></textarea>
+                <p v-if="error.feedback !== false" class="text-rose-500 text-xs">{{ error.feedback }}</p>
+                <div class="flex justify-end flex-col items-end grow pt-4">
+                    <button @click.prevent="submit"
+                        class="flex items-center justify-end gap-2 w-[116px] h-auto border bg-stone-200 text-stone-700 rounded px-4 py-2
+                        dark:bg-stone-950 dark:border-none dark:shadow-sm dark:text-stone-100">Submit
+                        <IconEdit class="w-6 h-6 lg:w-auto lg:h-8 dark:text-stone-100"></IconEdit></button>
+                </div>
             </div>
         </form>
-    </transition>
-</template>
+</transition></template>
